687d4d4eabb7d36969f55cec1daf2ef754af3043
Mrmaxxou <openrougeyron@gmail.com>
2023-02-20 18:55:37 +0100

    Déclaration const URL API & HTML

---
+++ front/js/script.js
@@ -0,0 +1,5 @@
+// Déclaration constante de l'URL de l'API
+const urlApi = 'http://localhost:3000/api/products'
+// Déclaration constante de l'URL HTML du fichier product.html
+const urlHtml = "./product.html"
+
