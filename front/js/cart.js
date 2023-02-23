b1a2f931c06928329216e7c59e4da470072d3386
Mrmaxxou <openrougeyron@gmail.com>
2023-02-23 14:53:09 +0100

    Intégration création boucle html produits description

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,11 +9,61 @@ const sectioncart = document.querySelector('#cart__items');
 
 cart.forEach((product) => {
 
+    
+
+    fetch(`http://localhost:3000/api/products/${product.id}`)
+    .then(response => response.json())
+    .then(productArticleId => {
+
+    
+    
+    // Création d'un élément article pour chaque éléments présent dans le localStorage
     const productArticle = document.createElement('article');
     productArticle.classList.add('cart__item');
     productArticle.dataset.id = product.id;
+    console.log(productArticle.dataset.id);
     productArticle.dataset.color= product.colors;
     sectioncart.appendChild(productArticle);
+
+    //Création d'une div comprenant l'image du produit
+    const productDivImg = document.createElement('div');
+    productDivImg.classList.add('cart__item__img');
+    productArticle.appendChild(productDivImg);
+        // Création d'un élément image et difinir l'attribut source et alt pour l'image de l'éléments de l'API
+        const productImg = document.createElement('img');
+        productImg.src = productArticleId.imageUrl;
+        productImg.alt = productArticleId.name;
+        productDivImg.appendChild(productImg);
+
+
+    // Création d'une div contenant les informations du produit
+    const productDivContent = document.createElement('div');
+    productDivContent.classList.add('cart__item__content');
+    productArticle.appendChild(productDivContent);
+        // Création d'une div contenant les descriptions du produit
+        const productDivContentDescription = document.createElement('div');
+        productDivContentDescription.classList.add('cart__item__content__description');
+        productDivContent.appendChild(productDivContentDescription);
+            // Création d'un élément 'h2' pour le nom du produit
+            const productName = document.createElement('h2');
+            productName.textContent = productArticleId.name;
+            productDivContentDescription.appendChild(productName);
+            // Création d'un élément 'p' pour la couleur du produit
+            const productColor = document.createElement('p'); 
+            productColor.textContent = productArticle.dataset.color;
+            productDivContentDescription.appendChild(productColor);
+            // Création d'un élément 'p' pour le prix du produit
+            const productPrice = document.createElement('p');
+            productPrice.textContent = productArticleId.price + '€';
+            productDivContentDescription.appendChild(productPrice);
+
+   
+
+
+
+
+    })
+    
 
 
 });
