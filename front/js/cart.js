323baafdfafa7a6a9ec56c5cd1e6e464c34e709a
Mrmaxxou <openrougeyron@gmail.com>
2023-02-23 13:35:32 +0100

    Création boucle création <article>

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,19 @@
+// Récupération de la liste Array dans le localeStorage
 const cart = JSON.parse(localStorage.getItem('cartProduct'));
 console.log(cart);
 
+// Sélection de l'élément HTML 
+const sectioncart = document.querySelector('#cart__items');
+
+// Création d'une boucle pour la création de chaque produits présent dans le panier
+
+cart.forEach((product) => {
+
+    const productArticle = document.createElement('article');
+    productArticle.classList.add('cart__item');
+    productArticle.dataset.id = product.id;
+    productArticle.dataset.color= product.colors;
+    sectioncart.appendChild(productArticle);
+
+
+});
